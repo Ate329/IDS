3f3ee72cd4da9a0ef57c6b1345c6543fe92e1ece
Ate329 <justin.he814@gmail.com>
2024-11-16 15:28:27 +0200

    update manage.py

--- ids_project/manage.py
+++ ids_project/manage.py
@@ -2,6 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import webbrowser
+from threading import Timer
+
+
+def open_browser():
+    """Open the default web browser after a short delay."""
+    webbrowser.open_new("http://127.0.0.1:8000")
 
 
 def main():
@@ -15,8 +22,13 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # Open the browser after starting the server
+    if len(sys.argv) > 1 and sys.argv[1] == "runserver":
+        Timer(3, open_browser).start()  # Wait 3 seconds before opening the browser
+
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
